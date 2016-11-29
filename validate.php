4d9715e83011218eee6a422841bc1c6c75c756ee
nicholas birchfield <nicholas birchfield>
2016-11-29 13:23:26 -0600

    m

--- validate.php
+++ validate.php
@@ -7,15 +7,15 @@
 	$username = stripcslashes($username);
 	$password = stripcslashes($password);
 
-	$username = mysql_real_escape_string($username);
-	$password = mysql_real_escape_string($password);
+	$username = mysqli_affected_rows($username);
+	$password = mysqli_real_escape_string($password);
 	
 	//connect to the server and select database
-	mysql_connect("localhost", "group4", "Group4@TSM");
-	mysql_select_db("group4");
+	mysqli_connect("localhost", "group4", "Group4@TSM");
+	mysqli_select_db("group4");
 
 	//Query the database for user
-	$results = mysql_query("select * from EmployeeTable where username = '$username' and password = '$password'")
-			or die("failed to query database " .mysql_error());
-	echo"$results"
+	$results = mysqli_query("select SUM(CASE WHEN Username = '$username' AND Password = '$password' THEN 1 ELSE 0) from EmployeeTable")
+			or die("failed to query database " .mysqli_error());
+	return $results;
 ?>
