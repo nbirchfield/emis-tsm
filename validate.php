aa8b79c9f58fc8a3b22ccceda379cfa2d61d99b4
nicholas birchfield <nicholas birchfield>
2016-11-29 20:10:37 -0600

    stuff

--- validate.php
+++ validate.php
@@ -16,9 +16,13 @@
 	//Query the database for user
 	if($results = mysqli_query($con,"select * from EmployeeTable")) {
         while($row = mysqli_fetch_array($results)) {
-            foreach ($row as $column)
-            	echo "before column<br />";
-                echo "$column<br />";
+			echo "$row[0]<br />";
+            echo "$row[1]<br />";
+            echo "$row[2]<br />";
+            echo "$row[3]<br />";
+            echo "$row[4]<br />";
+            echo "$row[5]<br />";
+
         }
     } else {
 		echo mysqli_errno($con);
