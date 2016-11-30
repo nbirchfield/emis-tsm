eef9f5caddd651726fd24c2d2b1c399cd9c89bf8
nicholas birchfield <nicholas birchfield>
2016-11-30 00:19:36 -0600

    stuff

--- validate.php
+++ validate.php
@@ -3,11 +3,11 @@
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
-	echo "after entry: $username<br/>";
 	//prevent mysq; injection
 	$username = stripcslashes($username);
 	$password = stripcslashes($password);
 
+	//Remove these because messing up variables can add something later
 	//$username = mysqli_affected_rows($username);
 	//$password = mysqli_real_escape_string($password);
 	
@@ -17,20 +17,15 @@
 	$stmt = mysqli_prepare($con, "select count(*) from EmployeeTable where username = ? and password = ?");
 	mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
 
-	echo "$username<br/>";
-
 	//Query the database for user
 	if(mysqli_stmt_execute($stmt)) {
-		echo "in query<br />";
 		mysqli_stmt_bind_result($stmt, $results);
 		mysqli_stmt_fetch($stmt);
-		echo "result: $results<br />";
 		mysqli_stmt_close($stmt);
-        //echo "$row[0]<br />";
-        //if($row[0] == 1) {
-		//	header("Location: http://galadriel.cs.utsa.edu/~group4/landingpage.php?username=".urlencode("$username"));
-		//	exit;
-        //}
+        if($results == 1) {
+			header("Location: http://galadriel.cs.utsa.edu/~group4/landingpage.php?username=".urlencode("$username"));
+			exit;
+        }
     } else {
 		echo mysqli_errno($con);
 	}
