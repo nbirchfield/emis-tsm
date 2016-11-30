3aead72f548dfbe17994a46b60bac99e4c58ffa7
nicholas birchfield <nicholas birchfield>
2016-11-29 20:07:42 -0600

    stuff

--- validate.php
+++ validate.php
@@ -2,10 +2,6 @@
 	//get values passed from form in login.php file
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	
-
-	echo "in validate: $username  \n";
-	echo "in validate: $password  \n";
 
 	//prevent mysq; injection
 	$username = stripcslashes($username);
@@ -16,15 +12,13 @@
 	
 	#connect to the server and select database
 	$con = mysqli_connect("localhost", "group4", "Group4@TSM", "group4");
-	#echo "$con";
 
-		
-	$results = 0;	
 	//Query the database for user
-	if($results = mysqli_query($con,"select count(*) from EmployeeTable")) {
+	if($results = mysqli_query($con,"select * from EmployeeTable")) {
         while($row = mysqli_fetch_array($results)) {
             foreach ($row as $column)
-                printf("$column<br />");
+            	echo "before column<br />";
+                echo "$column<br />";
         }
     } else {
 		echo mysqli_errno($con);
@@ -32,7 +26,4 @@
 	mysqli_free_result($results);
 	mysqli_close($con);
 	echo "here\n";
-
-	//echo "results:  $results";
-	return $results;
 ?>
