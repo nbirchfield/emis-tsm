585bcfa0e3ffce66c346f8dabadbf9203ba52dad
Nick <kingkongn64@hotmail.com>
2016-11-29 21:03:32 -0600

    stuff

---
+++ landingpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<style>
+body{
+    background-image: url("http://i.imgur.com/C57RUOa.png");
+    background-size:     cover;
+    background-repeat:   no-repeat;
+
+}
+</style>
+
+<body>
+
+</body>
+</html>
+
