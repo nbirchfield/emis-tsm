09be105f1852b623672b84a527768cdac90c7776
nicholas birchfield <nicholas birchfield>
2016-11-29 21:09:37 -0600

    stuff

--- validate.php
+++ validate.php
@@ -14,15 +14,9 @@
 	$con = mysqli_connect("localhost", "group4", "Group4@TSM", "group4");
 
 	//Query the database for user
-	if($results = mysqli_query($con,"select * from EmployeeTable")) {
-        while($row = mysqli_fetch_array($results)) {
-			echo "$row[0]<br />";
-            echo "$row[1]<br />";
-            echo "$row[2]<br />";
-            echo "$row[3]<br />";
-            echo "$row[4]<br />";
-            echo "$row[5]<br />";
-
+	if($results = mysqli_query($con, "select count(*) from EmployeeTable, PatientTable where username = $username and password = $password")) {
+        if(mysqli_fetch_array($results) == 1) {
+			header("Locatoin: http://galadriel.cs.utsa.edu/~group4/landingpage.php");
         }
     } else {
 		echo mysqli_errno($con);
