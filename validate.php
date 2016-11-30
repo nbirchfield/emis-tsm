da90eeecd8400465f5569b9e29dc907a19b244e3
nicholas birchfield <nicholas birchfield>
2016-11-30 00:06:42 -0600

    stuff

--- validate.php
+++ validate.php
@@ -12,15 +12,18 @@
 	
 	#connect to the server and select database
 	$con = mysqli_connect("localhost", "group4", "Group4@TSM", "group4");
-	$stmt = mysqli_prepare("select count(*) from EmployeeTable where username = ? and password = ?");
-	$stmt ->bind_param("ss", $username, $password);
+
+	$stmt = mysqli_prepare($con, "select count(*) from EmployeeTable where username = ? and password = ?");
+	mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+
 	//Query the database for user
-	if($stmt->execute()) {
+	if(mysqli_stmt_execute($stmt)) {
 		echo "in query<br />";
-		$stmt->bind_result($results);
-		$stmt->fetch();
-        $row = mysqli_fetch_array($results);
-        echo "$row[0]<br />";
+		mysqli_stmt_bind_result($stmt, $results);
+		mysqli_stmt_fetch($stmt);
+		echo "result: $results<br />";
+		mysqli_stmt_close($stmt);
+        //echo "$row[0]<br />";
         //if($row[0] == 1) {
 		//	header("Location: http://galadriel.cs.utsa.edu/~group4/landingpage.php?username=".urlencode("$username"));
 		//	exit;
@@ -28,7 +31,6 @@
     } else {
 		echo mysqli_errno($con);
 	}
-	mysqli_free_result($results);
 	mysqli_close($con);
 	echo "dammit didnt work\n";
 ?>
