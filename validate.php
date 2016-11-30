ee280d025d2788c45939e8d3080f7dde5a23e0c5
nicholas birchfield <nicholas birchfield>
2016-11-29 19:34:23 -0600

    stuff

--- validate.php
+++ validate.php
@@ -22,9 +22,10 @@
 	$results = 0;	
 	//Query the database for user
 	if($results = mysqli_query($con,"select count(*) from EmployeeTable")) {
-        //$row = mysqli_fetch_array($results);
-		//foreach($row as $column)
-			printf("$results<br />");
+        while($row = mysqli_fetch_array($results)) {
+            foreach ($row as $column)
+                printf("$column<br />");
+        }
     } else {
 		echo mysqli_errno($con);
 	}
