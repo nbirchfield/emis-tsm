f9a4800192d20edf4478a6017844c025d6818b53
Nick <kingkongn64@hotmail.com>
2016-11-30 20:42:43 -0600

    stuff

--- validatecreate.php
+++ validatecreate.php
@@ -36,19 +36,15 @@
         }
 	 if($results == 0) {
 		mysqli_free_result($results);
-		$addstmt = mysqli_prepare($con, "INSERT INTO PatientTableNew VALUES(?,?,?,?,?)");
+		$addstmt = mysqli_prepare($con, "INSERT INTO PatientTableNew VALUES(?,?,?,?,?,'0')");
 		mysqli_stmt_bind_param($addstmt, 'sssss', $firstname,$lastname,$username,$password,$email);
-//		mysqli_stmt_execute($addstmt));
-		echo "I GOT HERE\n";
-		echo "$firstname,$lastname,$username,$password,$email";
 		if(mysqli_stmt_execute($addstmt)){
 			mysqli_stmt_fetch($addstmt);
 			mysqli_stmt_close($addstmt);
 		
-		echo "I GOT HERE TOO";	
         	}
-		mysqli_close($con);
-//			header("Location: http://galadriel.cs.utsa.edu/~group4/landingpage.php");
+		mysqli_close($con)
+			header("Location: http://galadriel.cs.utsa.edu/~group4/landingpage.php");
 			exit;
         }
         else {
