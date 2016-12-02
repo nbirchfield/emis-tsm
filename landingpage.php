183b8b1f88bb072c2a1ac41912732fd49cb729eb
Nick <kingkongn64@hotmail.com>
2016-12-01 20:32:34 -0600

    stuff

--- landingpage.php
+++ landingpage.php
@@ -1,15 +1,74 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<title>LandingPage</title>
 <style>
-body{
-    background-image: url("http://i.imgur.com/C57RUOa.png");
-    background-size:     cover;
-    background-repeat:   no-repeat;
-
+body {
+    margin: 0;
+	max-width: 100%;
+	height: auto;
+	width: auto;
+	background-image: url("http://i.imgur.com/uO6RkYM.jpg");
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	
 }
-</style>
 
+h1{
+	font-family:Oswald;
+    margin-bottom:0px;
+    color: black ;
+	text-align: center;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 15%;
+    background-color: black;
+    position: fixed;
+    height: 100%;
+    overflow: auto;
+}
+
+li a {
+    display: block;
+    color: white;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+
+li a:hover{
+    background-color: #555;
+    color: white;
+}
+
+.page{
+	margin-left:25%;
+	padding:1px 16px;
+	height:99vh;
+}
+
+</style>
+</head>
 <body>
+
+<ul>
+  <li><a href="landingpage.php">Home</a></li>
+  <li><a href="">Calander</a></li>
+  <li><a href="">Make Appointment</a></li>
+  <li><a href="">Medical History</a></li>
+  <li><a href="">Messages</a></li>
+  <li><a href="">Personal Info</a></li>
+  <li><a href="">Pay Bills</a></li>
+</ul>
+
+<div class="page">
+<h1>Welcome</h1>
+
+</div>
 
 </body>
 </html>
