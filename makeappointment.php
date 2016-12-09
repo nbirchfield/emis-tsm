b3517e389694c3c0d07a7ca99dd8ccf36eac4246
nicholas birchfield <nicholas birchfield>
2016-12-09 00:58:28 -0600

    stuff

--- makeappointment.php
+++ makeappointment.php
@@ -3,7 +3,8 @@
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $date = $_POST['date'];
+    $time = $_POST['time'];
     $doctor = $_POST['doctor'];
 
-    echo "$firstname\n$lastname\n$date\n$doctor";
+    echo "$firstname\n$lastname\n$date\n$time\n$doctor";
 ?>
