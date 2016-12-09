5117ad1464d9aa13b590afa7b3b79197ed275607
nicholas birchfield <nicholas birchfield>
2016-12-09 01:15:20 -0600

    stuff

--- makeappointment.php
+++ makeappointment.php
@@ -5,6 +5,24 @@
     $date = $_POST['date'];
     $time = $_POST['time'];
     $doctor = $_POST['doctor'];
+    $datetime = $date. $time;
 
-    echo "$firstname\n$lastname\n$date\n$time\n$doctor";
+    echo "$firstname\n$lastname\n$datetime\n$doctor";
+
+    $firstname = stripcslashes($firstname);
+    $lastname = stripcslashes($lastname);
+
+    /*$con = mysqli_connect("localhost", "group4", "Group4@TSM", "group4");
+
+    # create sql query and bind parameters
+    $verifystmt = mysqli_prepare($con, "select SUM(CASE WHEN datetime = ? THEN 1 ELSE 0 END) from appointment");
+    mysqli_stmt_bind_param($verifystmt, 's', );
+
+    # Query the database to see i
+    if(mysqli_stmt_execute($verifystmt)) {
+        mysqli_stmt_bind_result($verifystmt, $results);
+        mysqli_stmt_fetch($verifystmt);
+        mysqli_stmt_close($verifystmt);
+    }
+    */
 ?>
