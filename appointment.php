0628f63ff880f9c584ab50e78e66621fdc56b5b5
nicholas birchfield <nicholas birchfield>
2016-12-13 13:44:49 -0600

    stuff

--- appointment.php
+++ appointment.php
@@ -126,17 +126,20 @@ form{
     $_SESSION["patientID"] = $result1;
 
     # create sql query and bind parameters
-    $verifystmt2 = mysqli_prepare($con, "select datetime from appointment limit 1");
-    #mysqli_stmt_bind_param($verifystmt2, 's', $result1);
+    $verifystmt2 = mysqli_prepare($con, "select appointmentID, datetime from appointment where patientID = ? limit 3");
+    mysqli_stmt_bind_param($verifystmt2, 's', $result1);
 
     echo "before second query";
     # Query the database to see i
     if(mysqli_stmt_execute($verifystmt2)) {
-        mysqli_stmt_bind_result($verifystmt2, $date_time);
+        mysqli_stmt_bind_result($verifystmt2, $result2);
 	    mysqli_stmt_fetch($verifystmt2);
 	    mysqli_stmt_close($verifystmt2);
         #$rownum = mysqli_num_rows($result2);
-	    echo "appointmentID = <br>date_time:$date_time";
+
+        while($row = mysqli_fetch_array($result2)) {
+            echo "appointmentID = ".$row['appointmentID']."\ndate_time:".$row['datetime']"<br>";
+        }
 
         #if(mysqli_num_rows($result2) > 0) {
 
