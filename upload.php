287639892929c9bec0144d0b2012256b80ee3ff9
nicholas birchfield <nicholas birchfield>
2016-12-13 15:46:12 -0600

    stuff

--- upload.php
+++ upload.php
@@ -5,12 +5,12 @@ if(isset($_POST['submit'])) {
 	$con = mysqli_connect("localhost", "group4", "Group4@TSM", "group4");
 
 	$imageName = mysqli_real_escape_string($_FILES["image"]["name"]);
-	$imageDate = mysqli_real_escape_string(file_get_contents($_FILES["image"]["tmp_name"]));
+	$imageData = mysqli_real_escape_string(file_get_contents($_FILES["image"]["tmp_name"]));
 	$imageType = mysqli_real_escape_string($_FILES["image"]["type"]);
 
 	#if(substr($imageType, 0, 5) == "image") {
-		echo "image type is : $imageType";
-		mysqli_query($con, "insert into uploads values (null,$imageName,$imageDate)");
+		echo $imageData;
+		mysqli_query($con, "insert into uploads values (null,$imageName,$imageData)");
 	/*} else {
 		echo "images only";
     }*/
